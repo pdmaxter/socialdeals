8fc70c3b31abbe19115258b3e9309554dd048398
Alok Maxter <apple@apples-MacBook-Pro.local>
2013-04-12 17:51:59 +0530

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialMediaDeals::Application.config.secret_token = '234fcddb4e4d11af60c66ea02ddb90c6d1d88bbaa4fe29efb254ae8d73b6e3f0683d0176fec8749f9f8e9c3dd683ebe23a949bb28ddabc72e556e548a3643cbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SocialMediaDeals::Application.config.session_store :cookie_store, key: '_SocialMediaDeals_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SocialMediaDeals::Application.config.session_store :active_record_store
